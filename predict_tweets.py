97ca81aa4a9f797a5590d591363be46db94bf98b
PolFRX <pol.2frx@hotmail.fr>
2020-12-03 17:13:45 +0100

    add verbose

--- predict_tweets.py
+++ predict_tweets.py
@@ -8,12 +8,14 @@ MODEL_NAME = 'sentiments_bert'
 
 subject = input('Entrez le sujet des tweets: ')
 
+print(f'- Start process')
 tweets = get_tweets(subject)
 predictions = predict(tweets, MODEL_NAME)
 
 positives = 0
 negatives = 0
 
+print(f'- Start computing the score')
 for prediction in predictions:
     pred = np.argmax(prediction)
     if pred == 0:
@@ -22,4 +24,4 @@ for prediction in predictions:
         positives += 1
 
 score = (((positives - negatives) / len(predictions)) + 1) * 50
-print(f'Le score de positivité {subject} est de {score}/100')
+print(f'\nLe score de positivité de "{subject}" est de {score}/100')
