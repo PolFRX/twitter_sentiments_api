40b41cd145effd993eca0725079571b5c7d5d676
PolFRX <pol.2frx@hotmail.fr>
2020-12-04 00:06:19 +0100

    add demoji for better removal of emojis

--- get_tweets.py
+++ get_tweets.py
@@ -3,18 +3,18 @@ from requests_oauthlib import OAuth1, OAuth2
 import requests
 import json
 import re
+import demoji
 
 
 def nlp_pipeline(text):
     text = re.sub(r"http\S+", "", text)
-    text = remove_emoji(text)
+    text = demoji.replace(text, '')
     text = re.sub(r"@(\w+)", ' ', text, flags=re.MULTILINE)
-    text = text.replace('RT', '')
     text = text.replace('#', '')
     text = text.lower()
     text = text.replace('\n', ' ').replace('\r', '')
     text = ' '.join(text.split())
-    text = re.sub(r"[A-Za-z\.]*[0-9]+[A-Za-z%°\.]*", "", text)
+    # text = re.sub(r"[A-Za-z\.]*[0-9]+[A-Za-z%°\.]*", "", text)
     text = re.sub(r"(\s\-\s|-$)", "", text)
     text = re.sub(r"[,\!\?\%\(\)\/\"]", "", text)
     text = re.sub(r"\&\S*\s", "", text)
@@ -26,31 +26,14 @@ def nlp_pipeline(text):
     text = re.sub(r"\%", "", text)
     text = re.sub(r"\:", "", text)
     text = re.sub(r"\@", "", text)
-    text = re.sub(r"\-", "", text)
 
     return text
-
-
-def remove_emoji(string):
-    emoji_pattern = re.compile("["
-                           u"\U0001F600-\U0001F64F"  # emoticons
-                           u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                           u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                           u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                           u"\U00002702-\U000027B0"
-                           u"\U000024C2-\U0001F251"
-                           "]+", flags=re.UNICODE)
-    return emoji_pattern.sub(r'', string)
 
 
 def get_tweets(subject):
     print(f'-- Start retrieving tweets')
     with open('credentials.json') as json_file:
         data = json.load(json_file)
-        api_key = data['api_key']
-        api_secret = data['api_secret']
-        user_token = data['user_token']
-        user_secret = data['user_secret']
         bearer_token = data['bearer_token']
 
     # auth = OAuth1(api_key, api_secret, user_token, user_secret)
@@ -79,6 +62,7 @@ def get_tweets(subject):
 
     dataset = []
     textt = []
+    demoji.download_codes()
     for tweet in data:
         textt.append(tweet["text"])
         txt = nlp_pipeline(tweet["text"])
@@ -89,4 +73,4 @@ def get_tweets(subject):
     return dataset
 
 
-get_tweets('#chasse')
+# get_tweets('#chasse')
