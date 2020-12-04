29375bc7eb50c7d8bf94716e806c34f7d1b555b6
PolFRX <pol.2frx@hotmail.fr>
2020-12-04 16:08:31 +0100

    add neutrals and pie chart

--- predict_tweets.py
+++ predict_tweets.py
@@ -1,6 +1,7 @@
 from get_tweets import get_tweets
 from model import predict
 import numpy as np
+from matplotlib import pyplot as plt
 
 
 MODEL_NAME = 'sentiments_bert'
@@ -14,14 +15,26 @@ predictions = predict(tweets, MODEL_NAME)
 
 positives = 0
 negatives = 0
+neutrals = 0
 
 print(f'- Start computing the score')
 for prediction in predictions:
     pred = np.argmax(prediction)
-    if pred == 0:
+    if -0.2 < prediction[0] < 0.2 and -0.2 < prediction[1] < 0.2:
+        neutrals += 1
+    elif pred == 0:
         negatives += 1
     else:
         positives += 1
 
 score = (((positives - negatives) / len(predictions)) + 1) * 50
-print(f'\nLe score de positivité de "{subject}" est de {score}/100')
+print(f'\nLe score de positivité de "{subject}" est de {"{:.2f}".format(score)}/100')
+
+labels = ['positives', 'negatives', 'neutrals']
+sizes = [positives, negatives, neutrals]
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+ax.axis('equal')
+ax.pie(sizes, labels=labels, autopct='%1.2f%%')
+plt.show()
+
